bbc0137204915977c3025bf0c8e59fed61a41b47
Guilherme de Lyra <guilyra12@gmail.com>
2020-12-29 03:20:11 -0200

    fixing bash scripts

--- entrypoints/test_app.sh
+++ entrypoints/test_app.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-go test -v ./tests/... -coverpkg=$(go list ./... | grep -v "/test" | tr "\n" ",") -coverprofile=coverage.txt -race -covermode=atomic
+go test -v ./tests/... -coverpkg="$(go list ./... | grep -v "/test" | tr "\n" ",")" -coverprofile=coverage.txt -race -covermode=atomic
 
 # go tool cover -html=coverage.out -o coverage.html
