60629ae3189e1590cfa21735b09766770ddc2c92
Guilherme de Lyra <guilherme.pereira@indicium.tech>
2021-10-19 09:34:53 -0300

    indenting file + modifying gitignore

--- server.go
+++ server.go
@@ -1,18 +1,19 @@
 package main
 
 import (
- "github.com/appointment-octopus/auth/routers"
- "github.com/appointment-octopus/auth/settings"
- "github.com/codegangsta/negroni"
- "net/http"
- "github.com/appointment-octopus/auth/core/db/migration"
+	"net/http"
+
+	"github.com/appointment-octopus/auth/core/db/migration"
+	"github.com/appointment-octopus/auth/routers"
+	"github.com/appointment-octopus/auth/settings"
+	"github.com/codegangsta/negroni"
 )
 
 func main() {
- settings.Init()
- migration.AutoMigration()
- router := routers.InitRoutes()
- n := negroni.Classic()
- n.UseHandler(router)
- http.ListenAndServe(":5000", n)
+	settings.Init()
+	migration.AutoMigration()
+	router := routers.InitRoutes()
+	n := negroni.Classic()
+	n.UseHandler(router)
+	http.ListenAndServe(":5000", n)
 }
